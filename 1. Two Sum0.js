91a513221d983c88c2ed1e1bfd56d9b14800a530
Mira M-Gh <55577054+miramgh@users.noreply.github.com>
2022-05-05 15:33:57 +0200

    Using double for loops

--- 1. Two Sum0.js
+++ 1. Two Sum0.js
@@ -6,7 +6,7 @@
  */
 /**
  * THIS IS NOT OPTIMAL => O(n)^2 as we used a double for loop 
- * THERE IS 2 ANOTHER SOLUTIONS USING (HASH TABLE "JS OBJECT - SORTING THE ARRAY )
+ * THERE IS 2 ANOTHER SOLUTIONS USING (HASH TABLE "JS OBJECT - SORTING THE ARRAY)
  */
 var twoSum = function(nums, target) {
     // We wanna go to the before last value in nums 
