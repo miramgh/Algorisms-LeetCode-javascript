9130f33852c4c4b5234af61a38f540f36e40ae8c
Mira M-Gh <55577054+miramgh@users.noreply.github.com>
2022-05-05 17:17:48 +0200

    Using hashTable "Object"

--- 1. Two Sum1.js
+++ 1. Two Sum1.js
@@ -21,4 +21,13 @@ var twoSum = (nums , target)=>{
    }return []
    
 };
+// Another way 
+var twoSum = (nums , target)=>{
+	let hashTable = {}
+    for(let i = 0; i < nums.length; i++) {
+        if (target-nums[i] in hashTable) {
+            return [hashTable[target-nums[i]], i]
+        }
+        hashTable[nums[i]] = i
+ }}
 //Conclusion : we loop through the array - add all the possible first numbers - check to see if a second number is present in the hash 
