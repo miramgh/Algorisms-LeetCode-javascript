a1a7f8fd713248d7c3852732b25424bedbd93d79
Mira M-Gh <55577054+miramgh@users.noreply.github.com>
2022-05-06 18:11:59 +0200

    Add Two Numbers

---
+++ 2. Add Two Numbers.js
@@ -0,0 +1,46 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var addTwoNumbers = function(l1, l2) {
+    let carry = 0 
+    let previousNode = new ListNode()
+    let headNode = previousNode 
+    // while l1 or l2 or carry is not null run this code 
+   while (l1 || l2 || carry ){
+       // The Two values we wanna sum 
+       let val1 = 0
+       let val2 = 0
+
+       if (l1){
+           val1 = l1.val
+           l1 = l1.next
+           
+       }
+       if (l2){
+           val2 = l2.val
+           l2 = l2.next 
+       }
+       let sum = val1 + val2 + carry
+       // if the result of sum is 10 or more update the carry to one 
+       carry = sum > 9 ? 1 : 0
+       const digit = sum % 10
+       // initialize a list that taking the carry value to sum this up with val1 & val2
+       const currentNode = new ListNode(digit)
+       
+       previousNode.next = currentNode
+        console.log(previousNode , currentNode)
+       previousNode = currentNode
+      
+   }
+    // At the end return the list contains all sums
+    return headNode.next
+};
